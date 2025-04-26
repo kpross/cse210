0840885e0a44fdfc3f32a49168c4b6089115247b
kpross <ros15001@byui.edu>
2025-04-26 13:52:31 -0600

    week one prep assignments

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        
+        Console.Write("What is your grade percentage? ");
+        string gps = Console.ReadLine();
+        int gp = int.Parse(gps);
+
+        string lg = "unknown";
+
+        if (gp >= 90)
+            {
+                lg = "A";
+            }
+        else if (gp >= 80)
+            {
+                lg = "B";
+            }
+            else if (gp >= 70)
+            {
+                lg = "C";
+            }
+            else if (gp >= 60)
+            {
+                lg = "D";
+            }
+            else if (gp <= 50)
+            {
+                lg = "F";
+            }
+        Console.WriteLine($"Your letter grade is is {lg}!");
+        if (gp >= 70)
+        {
+            Console.WriteLine("Congrats, You Passed!!!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, Try again next time");
+        }
     }
 }
