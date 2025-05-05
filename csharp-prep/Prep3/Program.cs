1c66a7a80b5d6eea0db1c81b00d3f2f99bc7ed3e
kpross <ros15001@byui.edu>
2025-05-05 13:03:11 -0600

    prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+        int guess = -20;
+
+        while (guess != number)
+        {
+            Console.Write("Guess a number between 1 and 100: \n");
+            guess = int.Parse(Console.ReadLine());
+            
+            if (guess > number)
+            Console.Write("Lower!\n");
+
+            else if (guess < number)
+            Console.Write("Higher!\n");
+
+            else
+            Console.Write("You Got It!!!\n");
+        }
     }
 }
